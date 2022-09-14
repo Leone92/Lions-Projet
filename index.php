ae6ad30c34ca3a2ad97dbb52fa997a6dbfd49105
Gennaro <Kabylus92@gmail.com>
2022-09-14 17:19:29 +0200

    first commit

--- index.php
+++ index.php
@@ -18,39 +18,10 @@
 include_once 'includes/header.php';
 ?>
 <!-- carousel -->
-<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-    <div class="carousel-indicators">
-        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
-            aria-current="true" aria-label="Slide 1"></button>
-        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
-            aria-label="Slide 2"></button>
-        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
-            aria-label="Slide 3"></button>
-    </div>
 
-    <div class="carousel-inner">
-        <div class="carousel-item active">
-            <img src="./Images/pexels-pixabay-416778 (2).jpg" class="d-block w-100" alt="...">
-        </div>
-        <div class="carousel-item">
-            <img src="./Images/pexels-gustavo-fring-6720446 (1).jpg" class="d-block w-100" alt="...">
-        </div>
-        <div class="carousel-item">
-            <img src="./Images/pexels-barbara-olsen-7869533 (1).jpg" class="d-block w-100" alt="...">
-        </div>
-    </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
-        data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
-        data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-    </button>
-</div>
 <!-- carousel -->
+    
+</body>
 
 
 <?php
@@ -58,3 +29,12 @@ include_once 'includes/header.php';
   require_once 'includes/footer.php';
 
 ?>
+
+<!-- git add .
+git commit -m "first commit" 
+git -u origin master-->
+
+<!-- carouel + img
+description equipements
+creation réseaux sociaux a href
+test police d'ecriture -->
